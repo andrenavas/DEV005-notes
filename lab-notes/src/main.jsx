faa9775ef081502155fc56863fc2297918259ea6
andrenavas <andreynanavas@gmail.com>
2023-07-20 16:05:46 -0500

    ADD: login form

--- lab-notes/src/main.jsx
+++ lab-notes/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import Login from './login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
+    <App />
   </React.StrictMode>,
 )
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,35 @@
-import './index.css'
+import './login.css'
+import MyNotesLogo from './assets/MyNotesLogo.png'
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log('Click a Login', data);
 
-    return (
-        <>
-        <h1>Welcome to "My Notes"</h1>
-        </>
-    )
+  return (
+    <div className='login-view'>
+      <header className='header-login'>
+        <h1 className='title-login'>WELCOME TO</h1>
+        <figure className='container-logo-login'>
+          <img src={MyNotesLogo} className='logo-login' alt='My Notes Logo' />
+        </figure>
+      </header>
+
+      <main className='container-form-login'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* Revisar si necesitamos agregar value y onChange*/}
+          <input type='email'{...register('email', { required: true })} placeholder='Email' />
+          {errors.email && <p>This file is required</p>}
+          <input type='password'{...register('password', { required: true })} placeholder='Password' />
+          {errors.password && <p>This file is required</p>}
+          <input type="submit" />
+        </form>
+
+      </main>
+
+
+
+    </div>
+  )
 }
 export default Login
