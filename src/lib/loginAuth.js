c51c034a000650ed0a9d84d0a07df88a5afcbaeb
Paulinavegac <paulina.vegac@gmail.com>
2023-07-28 17:17:47 -0400

    UPDATE: login function with email and password

--- src/lib/loginAuth.js
+++ src/lib/loginAuth.js
@@ -4,29 +4,28 @@ import { app } from "./firebase.js"
 // Initialize Firebase Authentication and get a reference to the service
  const auth = getAuth(app);
  export const loginApp = (email, password, loginError) => {
-signInWithEmailAndPassword(auth,email,password)
+ return signInWithEmailAndPassword(auth,email,password)
 .then((userCredential) => {
-    // Signed in 
     const user = userCredential.user;
     console.log(user)
-    // ...
+    return true
   })
   .catch((error) => {
     password = '';
       // Posibles errores de autenticación
       if (error.code === 'auth/wrong-password') {
-        loginError.textContent = 'Contraseña incorrecta';
+        loginError('Contraseña incorrecta');
       } else if (error.code === 'auth/user-not-found') {
-        loginError.textContent = 'El usuario no existe';
+        loginError('El usuario no existe');
       } else if (error.code === 'auth/invalid-email') {
-        loginError.textContent = 'Correo inválido';
+        loginError('Correo inválido');
       } else if (error.code === 'auth/too-many-requests') {
-        loginError.textContent = 'Demasiados intentos';
+        loginError('Demasiados intentos');
       } else if (error.code === 'auth/missing-password') {
-        loginError.textContent = 'Ingrese una contraseña';
+        loginError('Ingrese una contraseña');
       } else {
-        loginError.textContent = 'Ingresa los datos requeridos';
+        loginError('Ingresa los datos requeridos');
       }
-    
+      return false
   });
  }
