d434e980426f8189fa852926c5709e88e3e38df0
andrenavas <andreynanavas@gmail.com>
2023-07-28 16:39:37 -0500

    ADD: logout icon working

--- src/Wall.jsx
+++ src/Wall.jsx
@@ -2,32 +2,32 @@ import './wall.css'
 import MyNotesLogo from './assets/MyNotesLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom';
 
 const logoutIcon = <FontAwesomeIcon icon={faRightFromBracket} size="2xl" style={{ color: "#FFFF", }} />
 
 
 const Wall = () => {
-
+  const navigateTo = useNavigate();
+  const logout = () => {
+    navigateTo('/');
+  }
 
   return (
     <div className='wall-view'>
       <header className='header-wall'>
-      
         <h1 className='title-wall'>WELCOME TO</h1>
-        <div></div>
         <figure className='container-logo-wall'>
           <img src={MyNotesLogo} className='logo-wall' alt='My Notes Logo' />
         </figure>
+        <div className='container-logout' onClick={logout}> {logoutIcon}
+        </div>
       </header>
       <main className='container-wall'>
         <section className='container-post-wall'>
-          <span className='text-span-wall'>Developers working ...</span>          
+          <span className='text-span-wall'>Developers working ...</span>
         </section>
-
       </main>
-
-
-
     </div>
   )
 }
