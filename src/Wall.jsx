3388ebc258bfc78c6145db932fc98adfe22c2889
andrenavas <andreynanavas@gmail.com>
2023-07-28 16:46:28 -0500

    UPDATE: logout icon with styles

--- src/Wall.jsx
+++ src/Wall.jsx
@@ -16,7 +16,6 @@ const Wall = () => {
   return (
     <div className='wall-view'>
       <header className='header-wall'>
-        <h1 className='title-wall'>WELCOME TO</h1>
         <figure className='container-logo-wall'>
           <img src={MyNotesLogo} className='logo-wall' alt='My Notes Logo' />
         </figure>
